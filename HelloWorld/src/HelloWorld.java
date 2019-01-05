d6fe2944dae50cd54c6ec9db0f2ed80bb62bd20a
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2019-01-04 22:30:28 -0600

    Initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Please type something into the console.");
+		Scanner reader = new Scanner(System.in);
+		String input = reader.nextLine();
+		System.out.println("You typed: " + input);
+		reader.close();
+	}
+
+}
