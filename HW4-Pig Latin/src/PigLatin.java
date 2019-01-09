daf22f4314cfac3f531ca1e5834d8f24a6980fdd
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2019-01-08 23:02:19 -0600

    Finished rest of functionaities
    Need to refactor the code

--- HW4-Pig Latin/src/PigLatin.java
+++ HW4-Pig Latin/src/PigLatin.java
@@ -4,29 +4,74 @@ import java.util.Scanner;
 public class PigLatin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String pigLatinSartWithVowel= "";
+		String pigLatinStartWithCon = "";
+		String pigLatinWithVowel = "";
 		System.out.println("Please enter your word:");
 		Scanner reader = new Scanner(System.in);
 		String word = reader.nextLine();
 
-		String pigLatin = "";
+		pigLatinSartWithVowel = getPigLatinStartWithVowel(word);
+		System.out.println("pig latin start with vowel: " + pigLatinSartWithVowel);
+		pigLatinStartWithCon = getPigLatinStartWithCon(word);	
+		System.out.println("pig latin start with con: " + pigLatinStartWithCon);
+		pigLatinWithVowel = checkIfAnyVowel(word);	
+		System.out.println("pig latin start with con: " + pigLatinWithVowel);
 		
-		if (ifWordStartWithVowel(word)) {
-			pigLatin = word + "yay";
-		}
-		
-		System.out.println("pig latin: " + pigLatin);
 		reader.close();
 	}
-	
-	private static boolean ifWordStartWithVowel(String word) {
+
+	private static String getPigLatinStartWithVowel(String word) {
 		char firstChar = word.trim().charAt(0);
-		System.out.println("a:" + firstChar);
-		char[] vowels = {'a','e','i','o','u'};
-		boolean test = Arrays.asList(vowels).indexOf(firstChar) > -1;
-		System.out.println("a:" + Arrays.asList(vowels).indexOf('a'));
-		
-		return Arrays.asList(vowels).indexOf(firstChar) > -1 ? true : false;
+		char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+		for (char vowel : vowels) {
+			if (vowel == firstChar) {
+				return word + "yay";
+			}
+		}
+		return word;
 	}
 
+	private static String getPigLatinStartWithCon(String word) {
+		char[] chars = word.toCharArray();
+		char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+		String frontWord = "";
+		
+		while(chars.length > 0) {
+			boolean foundVowel = false;
+			for (char vowel : vowels) {
+				if(chars[0] == vowel) {
+					foundVowel = true;
+				}
+			}
+
+			if(foundVowel) {
+				return new String(chars) + frontWord + "ay";
+			}
+			else {
+				frontWord += chars[0];
+				chars = Arrays.copyOfRange(chars, 1, chars.length);
+			}
+		}
+		return word;
+	}
+	
+	private static String checkIfAnyVowel(String word) {
+		char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+		char[] wordChars = word.toCharArray();
+		boolean foundVowel = false;
+		
+		for(char wordChar : wordChars) {
+			for( char vowel :  vowels) {
+				if(wordChar == vowel) {
+					foundVowel = true;
+				}
+			}
+		}
+		
+		if (!foundVowel) {
+			return word + "ay";
+		}
+		return word;
+	}
 }
