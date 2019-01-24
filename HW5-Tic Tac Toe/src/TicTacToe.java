ae474d9bceb29e3d5b382f42e8ff6372508448bc
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2019-01-23 19:07:17 -0600

    Fixed two issues
        5 points because your game let me place pieces in spaces that were already occupied
        5 points because the game does not inform the user in the event of a tie/draw

--- HW5-Tic Tac Toe/src/TicTacToe.java
+++ HW5-Tic Tac Toe/src/TicTacToe.java
@@ -9,48 +9,86 @@ public class TicTacToe {
 		boolean continueGame = true;
 		Scanner reader = new Scanner(System.in);
 		String hand = "";
-		
+		int numberOfHands = 0;
+
 		while (continueGame) {
 			displayBoard();
 			System.out.println("Please enter your hand");
 			hand = reader.nextLine().trim();
-			char[] xyChar = hand.toCharArray();
-			if (isValidInput(xyChar)) {
-				int[] xy = { Integer.parseInt(Character.toString(xyChar[0]))
-							,Integer.parseInt(Character.toString(xyChar[1]))};
+			
+
+			if (isValidHand(hand)) {
+				char[] xyChar = hand.toCharArray();
+				int[] xy = { Integer.parseInt(Character.toString(xyChar[0])),
+						Integer.parseInt(Character.toString(xyChar[1])) };
+
 				placeHand(xy);
+				numberOfHands++;
 				boolean foundWinner = hasWinner(xy);
 				if (foundWinner == true) {
 					displayBoard();
 					System.out.println("Congrats Winner user " + playerTurn);
 					continueGame = false;
+				} else if (numberOfHands == 9) {
+					displayBoard();
+					System.out.println("This is a tied game!");
+					continueGame = false;
 				}
-			}else {
-				System.out.println(hand + " is not a valid hand. ");
-				System.out.println(hand + "Please input a valid hand such as 00, 01, 10, or so on.");
+
 			}
 		}
-		
+
 		reader.close();
+	}
+
+	private static boolean isValidHand(String hand) {
+		char[] xyChar = hand.toCharArray();
+		try {
+			Integer.parseInt(Character.toString(xyChar[0]));
+			Integer.parseInt(Character.toString(xyChar[1]));
+		} catch (NumberFormatException | NullPointerException | ArrayIndexOutOfBoundsException nfe) {
+			System.out.println(hand + " is not a valid hand. ");
+			System.out.println("Please input a valid hand such as 00, 01, 10, or so on.");
+			return false;
+		}
+
+		int[] xy = { Integer.parseInt(Character.toString(xyChar[0])),
+				Integer.parseInt(Character.toString(xyChar[1])) };
+
+		if (!isValidInput(xy)) {
+			System.out.println(hand + " is not a valid hand. ");
+			System.out.println("Please input a valid hand such as 00, 01, 10, or so on.");
+			return false;
+		}
+
+		if (checkIfPosIsTaken(xy)) {
+			System.out.println(hand + " has been taken. ");
+			System.out.println("Please input another hand.");
+			return false;
+		}
+
+		return true;
 	}
 
 	private static void placeHand(int[] xy) {
 		board[xy[0]][xy[1]] = playerTurn;
 	}
 
-	private static boolean isValidInput(char[] xy) {
+	private static boolean isValidInput(int[] xy) {
 		if (xy.length != 2) {
 			return false;
 		}
 
-		try {
-			Integer.parseInt(Character.toString(xy[0]));
-			Integer.parseInt(Character.toString(xy[1]));
-		} catch (NumberFormatException | NullPointerException nfe) {
-			return false;
+		return true;
+	}
+
+	private static boolean checkIfPosIsTaken(int[] xy) {
+
+		if (!board[xy[0]][xy[1]].equals(" ")) {
+			return true;
 		}
 
-		return true;
+		return false;
 	}
 
 	private static void displayBoard() {
