cb25bf2494b5bf5f470857616af45436a220a0e3
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2019-01-25 16:51:01 -0600

    Added comments

--- HW9-Driver's License/src/DriversLicense.java
+++ HW9-Driver's License/src/DriversLicense.java
@@ -1,8 +1,5 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Locale;
 
 public class DriversLicense {
 	private String _firstName;
@@ -10,7 +7,8 @@ public class DriversLicense {
 	private String _DOB;
 	private String _height;
 	private String _gender;
-
+	
+	/*Overload Construction to receive 5 parameters*/
 	public DriversLicense(String firstName, String lastName, String DOB, String height, String gender) {
 		_firstName = firstName;
 		_lastName = lastName;
@@ -18,18 +16,49 @@ public class DriversLicense {
 		_height = height;
 		_gender = gender;
 	}
-
+	
+	/*Get full name by concatenating first name and last name*/
 	public String getFullName() {
 		return _firstName + " " + _lastName;
 	}
 
-	public int getAge() {
-		LocalDate today = LocalDate.now(); // today's date
-		LocalDate DOBInDate = LocalDate.parse(_DOB);
+	/*Calculate ages by today's time minus driver's date of birth*/
+	public int getAge() {		
+		int age = -1;
 		
-		Period period = Period.between(DOBInDate, today);
-		int age = period.getYears(); // calculate the period between DOB and today in years
+		/*if the format of driver's date of birth is not correct, return error message to user and also set age to -1*/		
+		if(isValidDOBFormat()) {
+			LocalDate today = LocalDate.now(); // today's date
+			LocalDate DOBInDate = LocalDate.now();
+			
+			DOBInDate = LocalDate.parse(_DOB);		
+			Period period = Period.between(DOBInDate, today);
+			age = period.getYears(); // calculate the period between DOB and today in years	
+		}
 		
 		return age;
+	}
+	
+	private boolean isValidDOBFormat() {	
+		boolean validate = true;
+		
+		try {
+			LocalDate.parse(_DOB);		
+		}catch(Exception e) {
+			System.out.println("");
+			System.out.println(_DOB + " is NOT in correct format.");
+			System.out.println("Please use yyyy-mm-dd.");
+			validate = false;
+		}
+		
+		return validate;
+	}
+	
+	public String getHeight(){
+		return _height;
+	}
+	
+	public String getGender() {
+		return _gender;
 	}
 }
