c9d57a4a06802eff7acddb28b9df754a6fb8e23f
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2019-01-28 23:19:07 -0600

    Finished Parking Garage with functionalities and comments

--- HW10-Parking Garage/src/Car.java
+++ HW10-Parking Garage/src/Car.java
@@ -5,10 +5,18 @@ public class Car {
 	private String _make;
 	private String _model;
 
+	/*
+	 * Car class construction to initiate car properties with empty string if user
+	 * doesn't define
+	 */
 	Car() {
 		initiateCarProperties();
 	}
 
+	/*
+	 * Car class construction to initiate car properties with user defined
+	 * properties
+	 */
 	public Car(String color, String licenseNumber, String make, String model) {
 		_color = color;
 		_licenseNumber = licenseNumber;
@@ -16,10 +24,10 @@ public class Car {
 		_model = model;
 	}
 
-	public String getLicenseNumber() {
-		return _licenseNumber;
-	}
-
+	/*
+	 * Initiate/Reset car properties to be empty string which also means there is no
+	 * car assigned from user
+	 */
 	public void initiateCarProperties() {
 		_color = "";
 		_licenseNumber = "";
@@ -27,14 +35,22 @@ public class Car {
 		_model = "";
 	}
 
+	/* Return the car's color */
 	public String getColor() {
 		return _color;
 	}
 
+	/* Return the car's license number */
+	public String getLicenseNumber() {
+		return _licenseNumber;
+	}
+
+	/* Return the car's make */
 	public String getMake() {
 		return _make;
 	}
 
+	/* Return the car's model */
 	public String getModel() {
 		return _model;
 	}
--- HW10-Parking Garage/src/ParkingGarage.java
+++ HW10-Parking Garage/src/ParkingGarage.java
@@ -3,57 +3,94 @@ public class ParkingGarage {
 	private static int _capacity = 0;
 	Car[] _parkingSpots = new Car[_capacity];
 
+	/*
+	 * ParkingGarage Class construction to initiate the size of the parking garage
+	 */
 	public ParkingGarage(int capacity) {
 		_capacity = capacity;
+		/*
+		 * after having capacity from user, the _parkingSpots needs to be new up with
+		 * user's capacity
+		 */
 		_parkingSpots = new Car[_capacity];
 	}
 
 	/* will add the car to a parking spot. */
 	public void park(Car car, int spot) {
-		if (!doesSpotExist(car, spot) && isSpotTaken(spot)) {
+		/* check if the selected spot is inside of parking garage's capacity */
+		if (!isValidSpot(spot)) {
+			return;
+		}
+
+		/* If spot is taken by another car, the new car can't part at this spot */
+		if (isSpotTaken(spot, true)) {
 			return;
 		}
 
 		_parkingSpots[spot] = car;
 	}
 
-	private boolean doesSpotExist(Car car, int spot) {
+	/* check if the selected spot is inside of parking garage's capacity */
+	private boolean isValidSpot(int spot) {
 		try {
-			_parkingSpots[spot] = car;
+			if (spot > _parkingSpots.length - 1) {
+				throw new IndexOutOfBoundsException();
+			}
 		} catch (IndexOutOfBoundsException e) {
 			System.out.println("The spot #" + spot + " does not exist in this Garage.");
-			System.out.println("We only have the Spot #0 ~ #" + _parkingSpots.length);
+			System.out.println("Only the Spots #0 ~ #" + (_parkingSpots.length - 1) + " are available.");
+
 			return false;
 		}
 		return true;
 	}
 
-	private boolean isSpotTaken(int spot) {
-		if (_parkingSpots[spot].getLicenseNumber().equals(null)
-				&& _parkingSpots[spot].getLicenseNumber().isEmpty() == true) {
-			System.out.println("The spot #" + spot + " is taken.");
-			System.out.println("Please select another spot to park your car.");
+	/*
+	 * If spot is taken by another car, the new car can't part at this spot Since
+	 * vacate method calls this method to check if the car in this spot can be
+	 * unparked so the error message doesn't need to be printed out to user
+	 */
+	private boolean isSpotTaken(int spot, boolean printMessage) {
+		if (_parkingSpots[spot] == null) {
 			return false;
 		}
 
-		return true;
+		if (!_parkingSpots[spot].getLicenseNumber().isEmpty()) {
+			if (printMessage) {
+				System.out.println("The spot #" + spot + " is taken.");
+				System.out.println("Please choose another spot to park your car.");
+			}
+			return true;
+		}
+
+		return false;
 	}
 
-	/* will remove the car from the specified spot. */
+	/*
+	 * Remove the car from the specified spot by initiating the car property which
+	 * means creating an empty spot.
+	 */
 	public void vacate(int spot) {
-		if (!isSpotTaken(spot)) {
-			System.out.println("The spot #" + spot + " is empty.");
+		if (!isSpotTaken(spot, false)) {
+			System.out.println("The spot #" + spot + " is empty already.");
+
+			return;
 		}
 
 		_parkingSpots[spot].initiateCarProperties();
 	}
 
+	/* Print out all cars in the parking garage if the spot isn't empty */
 	public void printInventory() {
 		for (int i = 0; i < _parkingSpots.length; i++) {
-			System.out.println("The spot #" + i + " in the parking garage:");
-			System.out.println("The car's color " + _parkingSpots[i].getColor() + "; License# "
-					+ _parkingSpots[i].getLicenseNumber() + "; make " + _parkingSpots[i].getMake() + "; model "
-					+ _parkingSpots[i].getModel() + ".");
+			if (!_parkingSpots[i].getLicenseNumber().isEmpty()) {
+				System.out.println("The spot #" + i + " in the parking garage:");
+				System.out.println("    Make " + _parkingSpots[i].getMake() + "; Color " + _parkingSpots[i].getColor()
+						+ "; License# " + _parkingSpots[i].getLicenseNumber() + "; Model " + _parkingSpots[i].getModel()
+						+ ".");
+			}
 		}
+
+		System.out.println("");
 	}
 }
